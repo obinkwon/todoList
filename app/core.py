8cb242f28498f2b1ea5f531a89e536e21cb7dfe8
obkwon <obkwon@da-rum.com>
2024-12-07 00:52:15 +0900

    쿼리문 파일 추가

--- app/core.py
+++ app/core.py
@@ -2,6 +2,7 @@ import pymysql
 from utils.date import str_date
 from flask import jsonify
 from db.connection import get_connection
+from db.queries import SELECT_TODO_LIST, INSERT_TODO, UPDATE_TODO
 
 
 # 현재 날짜 가져오기
@@ -15,8 +16,7 @@ def load_todos():
     list = []
     with get_connection() as conn:
         with conn.cursor(pymysql.cursors.DictCursor) as cursor: # 결과를 딕셔너리 형태로 가져온다
-            query = "SELECT ID, TEXT, DATE, STATUS FROM TODOS WHERE DATE >= %s OR STATUS <> 'C'"
-            cursor.execute(query,(current_date))
+            cursor.execute(SELECT_TODO_LIST,(current_date))
             list = cursor.fetchall()
         return list
 
@@ -27,11 +27,9 @@ def save_todo(todo):
     with get_connection() as conn:
         with conn.cursor() as cursor:
             if todo.get('id') is None:
-                query = "INSERT TODOS INTO(TEXT, DATE, STATUS)VALUES(%s, %s, 'N')"
-                cursor.execute(query,(todo.get("text"),current_date))
+                cursor.execute(INSERT_TODO,(todo.get("text"),current_date))
             # 기존 데이터 수정일때
             else:
-                query = "UPDATE TODOS SET STATUS = (CASE WHEN STATUS = 'C' THEN 'N' ELSE 'C' END) WHERE ID = %s"
-                cursor.execute(query,(todo.get("id")))
+                cursor.execute(UPDATE_TODO,(todo.get("id")))
         conn.commit()
     return jsonify({'state': 'SUCCESS', 'message': 'success', 'todo': todo})
